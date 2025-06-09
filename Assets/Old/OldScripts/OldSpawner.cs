ebc15789e04ad22394fb4ffdcbc1464137dd9a13
Charlie Campbell <chacha9669@hotmail.com>
2025-06-09 16:25:20 +1000

    started setup for new movement

--- Assets/Old/OldScripts/OldSpawner.cs
+++ Assets/Old/OldScripts/OldSpawner.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public enum Team
 {
     TEAM_1,
-    TEAM_2
+    TEAM_2,
+    TEAM_3,
+    TEAM_4
 }
 
 
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,31 @@ using Unity.Mathematics;
 
 public struct Movement : IComponentData
 {
-    public float3 TargetPosition;
+    // type of enemy (ranged, melee, tank)
+    public NPCType npcType;
+    // reference to the enemy base this agent is trying to reach
+    private Entity enemyBase;
+    //current state of state machine
+    public NPCState currentState;
+    //speed to move towards the target
     public float MoveSpeed;
+    //how much damage this does when it attacks
+    public float damage;
+    //radius of the circle collision
+    public float enemyRangeDetection;
+    // the team this agent is on
+    private Team team;
+    // starting health
+    public float maximumHealth;
+    // current health
+    private float currentHealth;
+    // entity currently being targeted
+    public Entity currentTarget;
+    
+    
+    
+    
+    public float3 TargetPosition;
+    
 
 }
